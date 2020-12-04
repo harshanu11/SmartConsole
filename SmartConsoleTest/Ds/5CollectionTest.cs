22baa3f38605857feae184e67d5df7a8c81acd88
harsh singh <harshanu11@gmail.com>
2020-12-04 15:34:47 +0530

    2d array related problem

--- SmartConsoleTest/Ds/5CollectionTest.cs
+++ SmartConsoleTest/Ds/5CollectionTest.cs
@@ -7,7 +7,36 @@
 
             bool[] strArr = new bool[5];
             string[] strArr1 = new string[] { "ja", "tu" };
-            string[] strArr2 =  { "ja", "tu" };
+            string[] strArr2 = { "ja", "tu" };
+        }
+        public void TypsOfArr()
+        {
+            int[] arr = new int[] { 5, 7 };
+            int[][] jaggedArr = new int[3][];
+            int[,] twoDArr = new int[3, 3];
+            jaggedArr[0] = new int[3] { 1, 2, 3 };
+            jaggedArr[1] = new int[3] { 4, 5, 6 };
+            jaggedArr[2] = new int[3] { 7, 8, 9 };
+            int left_to_right = 0;
+            int reght_to_left = 0;
+            int row = jaggedArr.Length;
+            int columns = jaggedArr[0].Length;
+            int i = 0;
+            int j = 0;
+            int k = 0;
+            int l = jaggedArr.Length - 1;
+            while (i < row && j < columns && k < row && l >= 0)
+            {
+                left_to_right += jaggedArr[i][j];
+                reght_to_left += jaggedArr[k][l];
+                i++;
+                j++;
+                k++;
+                l--;
+            }
+
+            Console.WriteLine(Math.Abs(left_to_right - reght_to_left));
+
         }
     }
 }
