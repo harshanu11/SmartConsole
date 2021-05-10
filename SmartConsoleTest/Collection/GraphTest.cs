1639d493e50bb585dcdb17a7d7a0e2e35e358e99
harsh singh <harshanu11@gmail.com>
2021-05-10 19:19:14 +0530

    adding graph and tree

---
+++ SmartConsoleTest/Collection/GraphTest.cs
@@ -0,0 +1,114 @@
+﻿
+// C# program to check if there is
+// exist a path between two vertices
+// of a graph.
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Xunit;
+
+namespace CollectionTest
+{
+    public class GraphTest
+    {
+        /// <summary>
+        /// Find if there is a path between two vertices in a directed graph
+        /// </summary>
+        [Fact]
+        public void FindPathBwnTwoNodeGraphTest()
+        {
+            MyGraph g = new MyGraph(4);
+            g.addEdge(0, 1);
+            g.addEdge(0, 2);
+            g.addEdge(1, 2);
+            g.addEdge(2, 0);
+            g.addEdge(2, 3);
+            g.addEdge(3, 3);
+            int u = 1;
+            int v = 3;
+            if (g.isReachable(u, v))
+                Console.WriteLine("There is a path from " + u + " to " + v);
+            else
+                Console.WriteLine("There is no path from " + u + " to " + v);
+            u = 3;
+            v = 1;
+            if (g.isReachable(u, v))
+                Console.WriteLine("There is a path from " + u + " to " + v);
+            else
+                Console.WriteLine("There is no path from " + u + " to " + v);
+        }
+    }
+    public class MyGraph
+    {
+        private int V; // No. of vertices
+        private LinkedList<int>[] adj; //Adjacency List
+
+        // Constructor
+        public MyGraph(int v)
+        {
+            V = v;
+            adj = new LinkedList<int>[v];
+            for (int i = 0; i < v; ++i)
+                adj[i] = new LinkedList<int>();
+        }
+
+        // Function to add an edge into the graph
+        public void addEdge(int v, int w)
+        {
+            adj[v].AddLast(w);
+        }
+
+        // prints BFS traversal from a given source s
+        public bool isReachable(int s, int d)
+        {
+            // LinkedList<int> temp = new LinkedList<int>();
+
+            // Mark all the vertices as not visited(By default set
+            // as false)
+            bool[] visited = new bool[V];
+
+            // Create a queue for BFS
+            LinkedList<int> queue = new LinkedList<int>();
+
+            // Mark the current node as visited and enqueue it
+            visited[s] = true;
+            queue.AddLast(s);
+
+            // 'i' will be used to get all adjacent vertices of a vertex
+            IEnumerator i;
+            while (queue.Count != 0)
+            {
+
+                // Dequeue a vertex from queue and print it
+                s = queue.First.Value;
+                queue.RemoveFirst();
+                int n;
+                i = adj[s].GetEnumerator();
+
+                // Get all adjacent vertices of the dequeued vertex s
+                // If a adjacent has not been visited, then mark it
+                // visited and enqueue it
+                while (i.MoveNext())
+                {
+                    n = (int)i.Current;
+
+                    // If this adjacent node is the destination node,
+                    // then return true
+                    if (n == d)
+                        return true;
+
+                    // Else, continue to do BFS
+                    if (!visited[n])
+                    {
+                        visited[n] = true;
+                        queue.AddLast(n);
+                    }
+                }
+            }
+
+            // If BFS is complete without visited d
+            return false;
+        }
+
+    }
+}
--- SmartConsoleTest/Collection/TreeTest.cs
+++ SmartConsoleTest/Collection/TreeTest.cs
@@ -1,4 +1,5 @@
-﻿using Xunit;
+﻿using System;
+using Xunit;
 
 namespace CollectionTest
 {
@@ -19,6 +20,78 @@ namespace CollectionTest
             nums.Insert(67);
             nums.Insert(76);
             nums.Insert(72);
+
+
         }
+
+        [Fact]
+        public void SortedArrayToBalanceTreeTest()
+        {
+            TreeNodev1 root;
+            BinaryTreev1 tree = new BinaryTreev1();
+            int[] arr = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+            int n = arr.Length;
+            root = tree.sortedArrayToBST(arr, 0, n - 1);
+            Console.WriteLine("Preorder traversal of constructed BST");
+            tree.preOrder(root);
+        }
+
+    }
+
+    public class TreeNodev1
+    {
+
+        public int data;
+        public TreeNodev1 left, right;
+
+        public TreeNodev1(int d)
+        {
+            data = d;
+            left = right = null;
+        }
+    }
+    public class BinaryTreev1
+    {
+
+        public static TreeNodev1 root;
+
+        /* A function that constructs Balanced Binary Search Tree
+		from a sorted array */
+        public virtual TreeNodev1 sortedArrayToBST(int[] arr, int start, int end)
+        {
+
+            /* Base Case */
+            if (start > end)
+            {
+                return null;
+            }
+
+            /* Get the middle element and make it root */
+            int mid = (start + end) / 2;
+            TreeNodev1 node = new TreeNodev1(arr[mid]);
+
+            /* Recursively construct the left subtree and make it
+			left child of root */
+            node.left = sortedArrayToBST(arr, start, mid - 1);
+
+            /* Recursively construct the right subtree and make it
+			right child of root */
+            node.right = sortedArrayToBST(arr, mid + 1, end);
+
+            return node;
+        }
+
+        /* A utility function to print preorder traversal of BST */
+        public virtual void preOrder(TreeNodev1 node)
+        {
+            if (node == null)
+            {
+                return;
+            }
+            Console.Write(node.data + " ");
+            preOrder(node.left);
+            preOrder(node.right);
+        }
+
     }
 }
