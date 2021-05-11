4a31c3eb159aa63ac408dec789b08ce9e23be2d9
harsh singh <harshanu11@gmail.com>
2021-05-11 17:41:59 +0530

    restructuring

---
+++ SmartConsoleTest/Program/BitManipulation.cs
@@ -0,0 +1,298 @@
+﻿using System;
+using System.Text;
+using Xunit;
+
+namespace Program
+{
+    public class BitManipulation
+    {
+        [Fact]
+        public void BitManipulationTest()
+        {
+            int N = 1201, M = 8, i = 3, j = 6;
+            Console.Write("N = " + N + "(");
+            BitMani.bin(N);
+            Console.WriteLine(")");
+
+            // print original bitset
+            Console.Write("M = " + M + "(");
+            BitMani.bin(M);
+            Console.WriteLine(")");
+
+            // Call function to 
+            // insert M to N
+            N = BitMani.insertion(N, M, i, j);
+            Console.WriteLine("After inserting M " +
+                              "into N from 3 to 6");
+
+            // Print the inserted bitset
+            Console.Write("N = " + N + "(");
+            BitMani.bin(N);
+            Console.WriteLine(")");
+        }
+        [Fact]
+        public void BinaryToStringTest()
+        {
+            double num1 = 0.625; // Input value in Decimal 
+            String output = BinaryToString.printBinary(num1);
+            Console.WriteLine("(0 " + output + ") in base 2");
+
+            double num2 = 0.72;
+            output = BinaryToString.printBinary(num2);
+            Console.WriteLine("(" + output + ") ");
+        }
+        [Fact]
+        public void FlipBitToWinTest()
+        {
+            // input 1
+            Console.WriteLine(FlipBitToWin.flipBit(13));
+
+            // input 2
+            Console.WriteLine(FlipBitToWin.flipBit(1775));
+
+            // input 3
+            Console.WriteLine(FlipBitToWin.flipBit(15));
+        }
+        [Fact]
+        public void NextNumberWithSame1Test()
+        {
+            int n = 5; // input 1
+            Console.WriteLine(NextNumberWithSame1.getNext(n));
+
+            n = 8; // input 2
+            Console.Write(NextNumberWithSame1.getNext(n));
+        }
+        [Fact]
+        public void FlipIntAtoBTest()
+        {
+            int a = 10;
+            int b = 20;
+            Console.WriteLine(FlipIntAtoB.FlippedCount(a, b));
+        }
+        [Fact]
+        public void PairwiseSwapTest()
+        {
+            int x = 23; // 00010111
+
+            // Output is 43 (00101011)
+            Console.Write(PairwiseSwap.swapBits(x));
+        }
+    }
+
+    public class PairwiseSwap
+    {
+
+        // Function to swap even
+        // and odd bits
+        public static long swapBits(int x)
+        {
+            // Get all even bits of x
+            long even_bits = x & 0xAAAAAAAA;
+
+            // Get all odd bits of x
+            long odd_bits = x & 0x55555555;
+
+            // Right shift even bits
+            even_bits >>= 1;
+
+            // Left shift even bits
+            odd_bits <<= 1;
+
+            // Combine even and odd bits
+            return (even_bits | odd_bits);
+        }
+
+        // Driver program to test above function
+
+    }
+    public class FlipIntAtoB
+    {
+
+        // Function that count set bits
+        public static int countSetBits(int n)
+        {
+            int count = 0;
+            while (n != 0)
+            {
+                count++;
+                n &= (n - 1);
+            }
+            return count;
+        }
+
+        // Function that return
+        // count of flipped number
+        public static int FlippedCount(int a, int b)
+        {
+            // Return count of set
+            // bits in a XOR b
+            return countSetBits(a ^ b);
+        }
+    }
+    public class NextNumberWithSame1
+    {
+
+        // Main Function to find next
+        // smallest number bigger than n
+        public static int getNext(int n)
+        {
+
+            /* Compute c0 and c1 */
+            int c = n;
+            int c0 = 0;
+            int c1 = 0;
+
+            while (((c & 1) == 0) && (c != 0))
+            {
+                c0++;
+                c >>= 1;
+            }
+            while ((c & 1) == 1)
+            {
+                c1++;
+                c >>= 1;
+            }
+
+            // If there is no bigger number
+            // with the same no. of 1's
+            if (c0 + c1 == 31 || c0 + c1 == 0)
+                return -1;
+
+            // position of rightmost
+            // non-trailing zero
+            int p = c0 + c1;
+
+            // Flip rightmost non-trailing
+            // zero
+            n |= (1 << p);
+
+            // Clear all bits to the right
+            // of p
+            n &= ~((1 << p) - 1);
+
+            // Insert (c1-1) ones on the
+            // right.
+            n |= (1 << (c1 - 1)) - 1;
+
+            return n;
+        }
+    }
+        public class BitMani
+    {
+        public static void bin(long n)
+        {
+            if (n > 1)
+                bin(n / 2);
+            Console.Write(n % 2);
+        }
+
+        // Insert m into n
+        public static int insertion(int n, int m,
+                             int i, int j)
+        {
+            int clear_mask = -1 << (j + 1);
+            int capture_mask = (1 << i) - 1;
+
+            // Capturing bits from i-1 to 0
+            int captured_bits = n & capture_mask;
+
+            // Clearing bits from j to 0
+            n &= clear_mask;
+
+            // Shiftng m to align with n
+            m = m << i;
+
+            // Insert m into n
+            n |= m;
+
+            // Insert captured bits
+            n |= captured_bits;
+
+            return n;
+        }
+    }
+    public class BinaryToString
+    {
+        // Main function to convert Binary real number 
+        // to String 
+        public static String printBinary(double num)
+        {
+            // Check Number is Between 0 to 1 or Not 
+            if (num >= 1 || num <= 0)
+                return "ERROR";
+
+            StringBuilder binary = new StringBuilder();
+            binary.Append(".");
+
+            while (num > 0)
+            {
+                /* Setting a limit on length: 32 characters, 
+                If the number cannot be represented 
+                accurately in binary with at most 32 
+                character */
+                if (binary.Length >= 32)
+                    return "ERROR";
+
+                // Multiply by 2 in num to check it 1 or 0 
+                double r = num * 2;
+                if (r >= 1)
+                {
+                    binary.Append(1);
+                    num = r - 1;
+                }
+                else
+                {
+                    binary.Append(0);
+                    num = r;
+                }
+            }
+            return binary.ToString();
+        }
+    }
+    public class FlipBitToWin 
+    {
+        public static int flipBit(int a)
+        {
+            /* If all bits are l, binary representation
+            of 'a' has all 1s */
+            if (~a == 0)
+            {
+                return 8 * sizeof(int);
+            }
+
+            int currLen = 0, prevLen = 0, maxLen = 0;
+            while (a != 0)
+            {
+                // If Current bit is a 1
+                // then increment currLen++
+                if ((a & 1) == 1)
+                {
+                    currLen++;
+                }
+
+                // If Current bit is a 0 then
+                // check next bit of a
+                else if ((a & 1) == 0)
+                {
+                    /* Update prevLen to 0 (if next bit is 0)
+                    or currLen (if next bit is 1). */
+                    prevLen = (a & 2) == 0 ? 0 : currLen;
+
+                    // If two consecutively bits are 0
+                    // then currLen also will be 0.
+                    currLen = 0;
+                }
+
+                // Update maxLen if required
+                maxLen = Math.Max(prevLen + currLen, maxLen);
+
+                // Remove last bit (Right shift)
+                a >>= 1;
+            }
+
+            // We can always have a sequence of
+            // at least one 1, this is fliped bit
+            return maxLen + 1;
+        }
+    }
+}
--- SmartConsoleTest/Program/LeetCode.cs
+++ SmartConsoleTest/Program/LeetCode.cs
@@ -1,7 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using Xunit;
 
-namespace SmartConsoleTest.Program
+namespace Program
 {
     public class LeetCode
     {
@@ -12,6 +13,57 @@ namespace SmartConsoleTest.Program
             //minFlipToMakeStringAlternate("10010100");
             MinOperations("10010100");
 
+        }
+        /// <summary>
+        /// Longest Consecutive Subsequence  https://www.geeksforgeeks.org/longest-consecutive-subsequence/
+        /// </summary>
+        [Fact]
+        public static void LongestConsecutiveSubsequenceTest()
+        {
+            int[] arr = { 1, 9, 3, 10, 4, 20, 2 };
+            int n = arr.Length;
+
+            Console.WriteLine("Length of the Longest " +
+                              "contiguous subsequence is " +
+                              findLongestConseqSubseq(arr, n));
+
+        }
+        static int findLongestConseqSubseq(int[] arr,
+                                   int n)
+        {
+
+            // Sort the array
+            Array.Sort(arr);
+
+            int ans = 0, count = 0;
+
+            List<int> v = new List<int>();
+            v.Add(10);
+
+            // Insert repeated elements
+            // only once in the vector
+            for (int i = 1; i < n; i++)
+            {
+                if (arr[i] != arr[i - 1])
+                    v.Add(arr[i]);
+            }
+
+            // Find the maximum length
+            // by traversing the array
+            for (int i = 0; i < v.Count; i++)
+            {
+
+                // Check if the current element is
+                // equal to previous element +1
+                if (i > 0 && v[i] == v[i - 1] + 1)
+                    count++;
+                else
+                    count = 1;
+
+                // Update the maximum
+                ans = Math.Max(ans, count);
+            }
+            return ans;
         }
         public int MinOperations(string s)
         {
