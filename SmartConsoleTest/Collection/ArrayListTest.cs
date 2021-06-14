c4b0abc49f2efec17dbdee212dd32f3f8ef8f998
harsh singh <harshanu11@gmail.com>
2021-06-14 18:57:58 +0530

    refactoring

--- SmartConsoleTest/Collection/ArrayListTest.cs
+++ SmartConsoleTest/Collection/ArrayListTest.cs
@@ -5,30 +5,30 @@ using Xunit;
 
 namespace CollectionTest
 {
-    public class ArrayListTest 
-	{
-		[Fact]
-        public void ArrayListMethod() 
-		{
+    public class ArrayListTest
+    {
+        [Fact]
+        public void ArrayListMethod()
+        {
             ArrayList fruits = new ArrayList();
             fruits.Add("mango");
             fruits.Add("apple");
             fruits.Add("lemon");
 
-            var  query = fruits.Cast<string>().Select(fruit => fruit);
+            var query = fruits.Cast<string>().Select(fruit => fruit);
 
             ArrayList arrList = new ArrayList();
 
-			Console.WriteLine("Lets start array list");
+            Console.WriteLine("Lets start array list");
 
-			arrList.Add("Hi");
-			arrList.Add("I am Harsh");
+            arrList.Add("Hi");
+            arrList.Add("I am Harsh");
 
-			for (int i = 0; i < arrList.Count; i++)
-			{
-				Console.WriteLine(arrList[i]);
-			}
+            for (int i = 0; i < arrList.Count; i++)
+            {
+                Console.WriteLine(arrList[i]);
+            }
 
-		}
+        }
     }
 }
