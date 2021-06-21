e046fcfe440059d1ba7beae71ba9c6cbffbc5285
harsh singh <harshanu11@gmail.com>
2021-06-21 18:54:05 +0530

    updating dictionary

--- SmartConsoleTest/Collection/DictionaryTest.cs
+++ SmartConsoleTest/Collection/DictionaryTest.cs
@@ -8,7 +8,12 @@ namespace CollectionTest
         [Fact]
         public void BasicDictionaryTest()
         {
-            Dictionary<int, int> d = new Dictionary<int, int>();
+            Dictionary<int, int> d = new Dictionary<int, int>() ;
+            d.Add(4, 3);
+            d.TryAdd(4, 3);
+            d.ContainsValue(5);
+            d.ContainsKey(4);
+            var data =d.Keys;
         }
     }
 }
