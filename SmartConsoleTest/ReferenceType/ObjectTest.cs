1a2813f53e06f48fbe5e5f4fbd3fc0e2d276f52b
harsh singh <harshanu11@gmail.com>
2021-07-11 12:12:27 +0530

    Deep copy shallow copy system

--- SmartConsoleTest/ReferenceType/ObjectTest.cs
+++ SmartConsoleTest/ReferenceType/ObjectTest.cs
@@ -1,4 +1,5 @@
-﻿using Xunit;
+﻿using System;
+using Xunit;
 
 namespace ReferenceTypeTest
 {
@@ -16,10 +17,62 @@ namespace ReferenceTypeTest
             Assert.True(ReferenceEquals(o, o2));
             Assert.True(o2 == o);
         }
+		[Fact]
+		public void TestDeepCopy()
+		{
+			CopyClass o1 = new CopyClass();
+			o1.cl2 = new CopyClassTest();
+			CopyClass o2 = o1.DeepCopy();
+			o2.cl2 = new CopyClassTest();
+
+
+			o1.cl2 = new CopyClassTest();
+			o1.num = 1;
+			o2.num = 2;
+			o1.cl2.num2 = 11;
+			o2.cl2.num2 = 22;
+			Console.WriteLine(o1.num);
+			Console.WriteLine(o2.num);
+			Console.WriteLine(o1.cl2.num2);
+			Console.WriteLine(o2.cl2.num2);
+			Console.WriteLine("Hello");
+		}
     }
     public class MyObjectClass
     {
         public int Intprop = 0;
         public string MyProperty = "55";
     }
+	public class CopyClass
+	{
+		public int num
+		{
+			get;
+			set;
+		}
+
+		public CopyClassTest cl2
+		{
+			get;
+			set;
+		}
+
+		public CopyClass DeepCopy()
+		{
+			CopyClass cl = new CopyClass();
+			cl = (CopyClass)this.MemberwiseClone();
+			cl.cl2 = new CopyClassTest();
+			cl.num = 0;
+			return cl;
+		}
+	}
+
+	public class CopyClassTest
+	{
+		public int num2
+		{
+			get;
+			set;
+		}
+	}
 }
