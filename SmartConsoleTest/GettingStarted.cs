031034cbfcc6826819c508d09960e20dd7e4472c
harsh singh <harshanu11@gmail.com>
2021-08-06 14:08:54 +0530

    code update

--- SmartConsoleTest/GettingStarted.cs
+++ SmartConsoleTest/GettingStarted.cs
@@ -21,10 +21,13 @@ namespace SmartConsoleTest
                         };
             int[,] c5 = new int[3, 5];
             int[][] c6 = new int[3][];
+            string str = "hell you";
             HashSet<int> hs = new HashSet<int>();
             string[] words =
                { "the", "fox", "jumps", "over", "the", "dog" };
-            LinkedList<string> sentence = new LinkedList<string>(words);
+            LinkedList<string> ll = new LinkedList<string>(words);
+            Stack<int> stack = new Stack<int>();
+            Queue<int> q = new Queue<int>();
             //Console.WriteLine(c4[0,2]);	Console.WriteLine(c4.Length);Console.WriteLine(c5.GetLength(1));		Console.WriteLine(c5.GetLength(0));
 
 
@@ -35,26 +38,30 @@ namespace SmartConsoleTest
             hs.Add(3);
             hs.Contains(3);
             hs.Remove(3);
-            sentence.AddFirst("today");
-            LinkedListNode<string> mark1 = sentence.First;
-            LinkedListNode<string> markPrev = sentence.Last.Previous;
-            var current = sentence.Find("fox");
-            if (sentence.Contains("jumps"))
+            ll.AddFirst("today");
+            LinkedListNode<string> mark1 = ll.First;
+            LinkedListNode<string> markPrev = ll.Last.Previous;
+            var current = ll.Find("fox");
+            if (ll.Contains("jumps"))
             {
-                sentence.RemoveFirst();
-                sentence.AddLast("yesterday");
+                ll.RemoveFirst();
+                ll.AddLast("yesterday");
 
             }
-            sentence.AddBefore(current, "quick");
-            current = sentence.Find("dog");
-            ICollection<string> icoll = sentence;
+            ll.AddBefore(current, "quick");
+            current = ll.Find("dog");
+            ICollection<string> icoll = ll;
+            stack.Push(4);
+            stack.Pop();
+            q.Enqueue(3);
+            q.Dequeue();
             //Console.WriteLine(c3[2]);
 
-            // sort reverse 
+            // sort reverse  copy
             Array.Sort(c3); Array.Sort(c3, 1, 2);
             Array.Reverse(c3);
-            // copy
             Array.Copy(c3, 0, c1, 0, 3);
+            ll.CopyTo(new string[100], 0);
 
             // resize
             Array.Resize(ref c3, 10);
@@ -63,7 +70,8 @@ namespace SmartConsoleTest
             st = string.Join("", c3);
             // find 
             var result = Array.Find(c3, x => x == 'x');
-            sentence.CopyTo(new string[100], 0);
+            String ans = str.Substring(4, str.Length - 4) + str.Substring(0, 4);
+
         }
     }
 }
