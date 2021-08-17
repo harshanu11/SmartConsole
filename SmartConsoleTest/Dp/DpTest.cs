7b737189181532fb668b19f97fdcab4ff210d40c
harsh singh <harshanu11@gmail.com>
2021-08-17 17:02:54 +0530

    code update

--- SmartConsoleTest/Dp/DpTest.cs
+++ SmartConsoleTest/Dp/DpTest.cs
@@ -14,6 +14,55 @@ namespace SmartConsoleTest.Dp
             var ans = DpOps.AllSum(8, new int[] { 2, 3, 5, 8 });
             var ertert = DpOps.allSumList;
             var ans1 = DpOps.BestSumSumt(7, new int[] { 5, 3, 4 });
+
+            countHopt(4, new int[] { 1, 2, 3 });//DistinctOccurrences("banana", new string[] {"ban" ,"b","ba"});
+
+        }
+
+        [Fact]
+        public void DPGeek()
+        {
+            countHopt(4, new int[] { 1, 2, 3 });//DistinctOccurrences("banana", new string[] {"ban" ,"b","ba"});
+
+        }
+        public static int countHop(int num, int[] hops)
+        {
+            int ans = 0;
+            if (num < 0) return 0;
+            if (num == 0) return 1;
+            ans = countHop(num - 1, hops) + countHop(num - 2, hops) + countHop(num - 3, hops);
+            return ans;
+        }
+
+        public static int countHopt(int num, int[] hops)
+        {
+            int ans = 0;
+            if (num < 0) return 0;
+            if (num == 0) return 1;
+            int[] table = new int[num + 1];
+
+            // seed 
+            table[0] = 1;
+            for (int t = 0; t < table.Length; t++)
+            {
+                if (table[t] != 0)
+                {
+                    if (t + 1 < table.Length)
+                    {
+                        table[t + 1] += table[t];
+                    }
+                    if (t + 2 < table.Length)
+                    {
+                        table[t + 2] += table[t];
+                    }
+                    if (t + 3 < table.Length)
+                    {
+                        table[t + 3] += table[t];
+                    }
+                }
+
+            }
+            return table[num];
         }
     }
 }
