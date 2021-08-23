68d29f47871f56423d60706f6ffdc5100810e741
harsh singh <harshanu11@gmail.com>
2021-08-23 12:06:20 +0530

    adding saperae section for interview

--- SmartConsole/Program.cs
+++ SmartConsole/Program.cs
@@ -1,87 +1,10 @@
-﻿using Program;
-using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Linq;
-
-namespace SmartConsole
+﻿namespace SmartConsole
 {
 
     public class Program
     {
         public static void Main()
         {
-            //decodeString(3, "mnes__ya_____mi");
-            //solution(3, "mnes__ya_____mi");
-            //var ans = isminimumNumberOfCoins(new int[] { 2, 5, 3, 6 },4, 1);
-            //var ans = subsequenceCount("banana", new string[] { "b", "ba", "a","an","ban","ana" });
-        }
-
-
-        public static String solution(int numberOfRows, String encodedString)
-        {
-            int originalLength = encodedString.Length;
-            char[] arr = new char[((originalLength / numberOfRows) + 1) * numberOfRows];
-
-            for (int i = 0; i < originalLength; i++)
-            {
-                arr[i] = encodedString[i];
-            }
-
-            int length = encodedString.Length;
-            int arrLength = length / numberOfRows;
-            char[] result = new char[length];
-            for (int i = 0; i < length; i++)
-            {
-                result[i] = '_';
-            }
-
-            StringBuilder decodedString = new StringBuilder();
-            int numberOfCols = originalLength / numberOfRows + 1;
-            int loop = numberOfCols - numberOfRows + 1;
-
-            for (int j = 0; j < loop; j++)
-            {
-                for (int i = 0; i < numberOfRows; i++)
-                {
-                    decodedString.Append(arr[i * numberOfCols + j] == '_' ? ' ' : arr[i * numberOfCols + j]);
-                }
-            }
-
-            return decodedString.ToString().Trim();
-        }
-        public static String decodeString(int numberOfRows, String encodedString)
-        {
-            if (encodedString == null)
-            {
-                return null;
-            }
-            int length = encodedString.Length;
-            int arrLength = length / numberOfRows;
-            char[] result = new char[length];
-            for (int i = 0; i < length; i++)
-            {
-                result[i] = '_';
-            }
-            int k = 0;
-            int newIndex = 0;
-            for (int i = 0; i < arrLength; i++)
-            {
-                result[k++] = encodedString[i];
-                while (newIndex <= length)
-                {
-                    newIndex = newIndex + arrLength + 1;
-                    if (newIndex < length)
-                    {
-                        result[k++] = encodedString[newIndex];
-                    }
-                }
-                newIndex = i + 1;
-
-            }
-            String decodedString = new String(result);
-            decodedString = decodedString.Replace("_", " ");
-            return decodedString.Trim();
         }
     }
 
---
+++ SmartConsoleTest/Interview/Amazon.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace SmartConsoleTest.Interview
+{
+    public class Amazon
+    {
+        [Fact]
+        public void TwoDArrayTest()
+        {
+            decodeString(3, "mnes__ya_____mi");
+            solution(3, "mnes__ya_____mi");
+
+
+        }
+        public static String solution(int numberOfRows, String encodedString)
+        {
+            int originalLength = encodedString.Length;
+            char[] arr = new char[((originalLength / numberOfRows) + 1) * numberOfRows];
+
+            for (int i = 0; i < originalLength; i++)
+            {
+                arr[i] = encodedString[i];
+            }
+
+            int length = encodedString.Length;
+            int arrLength = length / numberOfRows;
+            char[] result = new char[length];
+            for (int i = 0; i < length; i++)
+            {
+                result[i] = '_';
+            }
+
+            StringBuilder decodedString = new StringBuilder();
+            int numberOfCols = originalLength / numberOfRows + 1;
+            int loop = numberOfCols - numberOfRows + 1;
+
+            for (int j = 0; j < loop; j++)
+            {
+                for (int i = 0; i < numberOfRows; i++)
+                {
+                    decodedString.Append(arr[i * numberOfCols + j] == '_' ? ' ' : arr[i * numberOfCols + j]);
+                }
+            }
+
+            return decodedString.ToString().Trim();
+        }
+        public static String decodeString(int numberOfRows, String encodedString)
+        {
+            if (encodedString == null)
+            {
+                return null;
+            }
+            int length = encodedString.Length;
+            int arrLength = length / numberOfRows;
+            char[] result = new char[length];
+            for (int i = 0; i < length; i++)
+            {
+                result[i] = '_';
+            }
+            int k = 0;
+            int newIndex = 0;
+            for (int i = 0; i < arrLength; i++)
+            {
+                result[k++] = encodedString[i];
+                while (newIndex <= length)
+                {
+                    newIndex = newIndex + arrLength + 1;
+                    if (newIndex < length)
+                    {
+                        result[k++] = encodedString[newIndex];
+                    }
+                }
+                newIndex = i + 1;
+
+            }
+            String decodedString = new String(result);
+            decodedString = decodedString.Replace("_", " ");
+            return decodedString.Trim();
+        }
+    }
+}
