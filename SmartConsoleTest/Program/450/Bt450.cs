3113c0dd42fae5f091c6975a3e3eb5718039f1c8
harsh singh <harshanu11@gmail.com>
2021-09-10 16:51:40 +0530

    TreeUpdate

--- SmartConsoleTest/Program/450/Bt450.cs
+++ SmartConsoleTest/Program/450/Bt450.cs
@@ -1,23 +1,16 @@
 ﻿using System.Diagnostics;
 using System;
 using Xunit;
+using CollectionTest;
+using System.Collections.Generic;
 
 namespace DSA450
 {
     public class Bt450 
     {
         #region level order traversal
-        public class BTNode1
-        {
-            public int data;
-            public BTNode1 left, right;
-            public BTNode1(int item)
-            {
-                data = item;
-                left = right = null;
-            }
-        }
-        public BTNode1 btRoot;
+
+        public TreeNode btRoot;
         void printLevelOrder()
         {
             int h = height1(btRoot);
@@ -28,19 +21,16 @@ namespace DSA450
             }
         }
 
-        public int height1(BTNode1 root)
+        public int height1(TreeNode root)
         {
-            if (btRoot == null)
+            if (root == null)
             {
                 return 0;
             }
             else
             {
-                /* compute height of each subtree */
-                int lheight = height1(root.left);
-                int rheight = height1(root.right);
-
-                /* use the larger one */
+                int lheight = height1(root.Left);
+                int rheight = height1(root.Right);
                 if (lheight > rheight)
                 {
                     return (lheight + 1);
@@ -51,8 +41,27 @@ namespace DSA450
                 }
             }
         }
+        void printTreeLevelOrder1(TreeNode root)
+        {
+            Queue<TreeNode> queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+            while (queue.Count != 0)
+            {
+                TreeNode tempNode = queue.Dequeue();
+                Console.Write(tempNode.Data + " ");
 
-        public virtual void printCurrentLevel(BTNode1 root, int level)
+                if (tempNode.Left != null)
+                {
+                    queue.Enqueue(tempNode.Left);
+                }
+                if (tempNode.Right != null)
+                {
+                    queue.Enqueue(tempNode.Right);
+                }
+            }
+        }
+
+        void printCurrentLevel(TreeNode root, int level)
         {
             if (root == null)
             {
@@ -60,35 +69,35 @@ namespace DSA450
             }
             if (level == 1)
             {
-                Console.Write(root.data + " ");
+                Console.Write(root.Data + " ");
             }
             else if (level > 1)
             {
-                printCurrentLevel(root.left, level - 1);
-                printCurrentLevel(root.right, level - 1);
+                printCurrentLevel(root.Left, level - 1);
+                printCurrentLevel(root.Right, level - 1);
             }
         }
 
         [Fact]
         public void LevelOrderTest()
         {
-            this.btRoot = new BTNode1(1);
-            this.btRoot.left = new BTNode1(2);
-            this.btRoot.right = new BTNode1(3);
-            this.btRoot.left.left = new BTNode1(4);
-            this.btRoot.left.right = new BTNode1(5);
+            this.btRoot = new TreeNode(1);
+            this.btRoot.Left = new TreeNode(2);
+            this.btRoot.Right = new TreeNode(3);
+            this.btRoot.Left.Left = new TreeNode(4);
+            this.btRoot.Left.Right = new TreeNode(5);
             Debug.WriteLine("Level order traversal " +
                                 "of binary tree is ");
             this.printLevelOrder();
+            // second aproach 
+            printTreeLevelOrder1(btRoot);
         }
         #endregion
         #region Reverse Level Order traversal
         public class BinaryTree
         {
-            public BTNode1 root;
-
-
-            void reverseLevelOrder(BTNode1 node)
+            public TreeNode root;
+            void reverseLevelOrder(TreeNode node)
             {
                 int h = height(node);
                 int i;
@@ -100,34 +109,27 @@ namespace DSA450
                     printGivenLevel(node, i);
                 }
             }
-
-
-            void printGivenLevel(BTNode1 node, int level)
+            void printGivenLevel(TreeNode node, int level)
             {
                 if (node == null)
                     return;
                 if (level == 1)
-                    Console.Write(node.data + " ");
+                    Console.Write(node.Data + " ");
                 else if (level > 1)
                 {
-                    printGivenLevel(node.left, level - 1);
-                    printGivenLevel(node.right, level - 1);
+                    printGivenLevel(node.Left, level - 1);
+                    printGivenLevel(node.Right, level - 1);
                 }
             }
-
-            /* Compute the "height" of a tree --
-            the number of nodes along the longest
-            path from the root node down to the
-            farthest leaf node.*/
-            int height(BTNode1 node)
+            int height(TreeNode node)
             {
                 if (node == null)
                     return 0;
                 else
                 {
                     /* compute the height of each subtree */
-                    int lheight = height(node.left);
-                    int rheight = height(node.right);
+                    int lheight = height(node.Left);
+                    int rheight = height(node.Right);
 
                     /* use the larger one */
                     if (lheight > rheight)
@@ -144,11 +146,11 @@ namespace DSA450
 
                 // Let us create trees shown
                 // in above diagram
-                tree.root = new BTNode1(1);
-                tree.root.left = new BTNode1(2);
-                tree.root.right = new BTNode1(3);
-                tree.root.left.left = new BTNode1(4);
-                tree.root.left.right = new BTNode1(5);
+                tree.root = new TreeNode(1);
+                tree.root.Left = new TreeNode(2);
+                tree.root.Right = new TreeNode(3);
+                tree.root.Left.Left = new TreeNode(4);
+                tree.root.Left.Right = new TreeNode(5);
 
                 Debug.WriteLine("Level Order traversal " +
                                     "of binary tree is : ");
@@ -158,15 +160,15 @@ namespace DSA450
         #endregion
         #region Height of a tree
 
-        int maxDepth(BTNode1 node)
+        int maxDepth(TreeNode node)
         {
             if (node == null)
                 return 0;
             else
             {
                 /* compute the depth of each subtree */
-                int lDepth = maxDepth(node.left);
-                int rDepth = maxDepth(node.right);
+                int lDepth = maxDepth(node.Left);
+                int rDepth = maxDepth(node.Right);
 
                 /* use the larger one */
                 if (lDepth > rDepth)
@@ -181,11 +183,11 @@ namespace DSA450
         {
             BinaryTree tree = new BinaryTree();
 
-            tree.root = new BTNode1(1);
-            tree.root.left = new BTNode1(2);
-            tree.root.right = new BTNode1(3);
-            tree.root.left.left = new BTNode1(4);
-            tree.root.left.right = new BTNode1(5);
+            tree.root = new TreeNode(1);
+            tree.root.Left = new TreeNode(2);
+            tree.root.Right = new TreeNode(3);
+            tree.root.Left.Left = new TreeNode(4);
+            tree.root.Left.Right = new TreeNode(5);
 
             Debug.WriteLine("Height of tree is : " +
                                         maxDepth(tree.root));
@@ -195,15 +197,15 @@ namespace DSA450
         class Solution
         {
             // Function to find the diameter of a Binary Tree.
-            int diameterUtil(BTNode1 n, ref int dia)
+            int diameterUtil(TreeNode n, ref int dia)
             {
                 // if node becomes null, we return 0.
                 if (n == null) return 0;
 
-                // calling the function recursively for the left and
-                // right subtrees to find their heights.
-                int l = diameterUtil(n.left, ref dia);
-                int r = diameterUtil(n.right, ref dia);
+                // calling the function recursively for the Left and
+                // Right subtrees to find their heights.
+                int l = diameterUtil(n.Left, ref dia);
+                int r = diameterUtil(n.Right, ref dia);
 
                 // storing the maximum possible value of l+r+1 in diameter.
                 if (l + r + 1 > dia) dia = l + r + 1;
@@ -212,7 +214,7 @@ namespace DSA450
                 return 1 + Math.Max(l, r);
             }
             // Function to return the diameter of a Binary Tree.
-            public int diameter(BTNode1 root)
+            public int diameter(TreeNode root)
             {
                 int dia = 0;
                 // calling the function to find the result.
@@ -223,36 +225,36 @@ namespace DSA450
         }
         #endregion
         #region Mirror of a tree
-        public static BTNode1 createNode(int val)
+        public static TreeNode createNode(int val)
         {
-            BTNode1 newNode = new BTNode1(0);
-            newNode.data = val;
-            newNode.left = null;
-            newNode.right = null;
+            TreeNode newNode = new TreeNode(0);
+            newNode.Data = val;
+            newNode.Left = null;
+            newNode.Right = null;
             return newNode;
         }
 
-        public static void inorder(BTNode1 root)
+        public static void inorder(TreeNode root)
         {
             if (root == null)
             {
                 return;
             }
-            inorder(root.left);
-            Console.Write("{0:D} ", root.data);
-            inorder(root.right);
+            inorder(root.Left);
+            Console.Write("{0:D} ", root.Data);
+            inorder(root.Right);
         }
 
-        public static BTNode1 mirrorify(BTNode1 root)
+        public static TreeNode mirrorify(TreeNode root)
         {
             if (root == null)
             {
                 return null;
 
             }
-            BTNode1 mirror = createNode(root.data);
-            mirror.right = mirrorify(root.left);
-            mirror.left = mirrorify(root.right);
+            TreeNode mirror = createNode(root.Data);
+            mirror.Right = mirrorify(root.Left);
+            mirror.Left = mirrorify(root.Right);
             return mirror;
         }
 
@@ -260,16 +262,16 @@ namespace DSA450
         public void inorderTest()
         {
 
-            BTNode1 tree = createNode(5);
-            tree.left = createNode(3);
-            tree.right = createNode(6);
-            tree.left.left = createNode(2);
-            tree.left.right = createNode(4);
+            TreeNode tree = createNode(5);
+            tree.Left = createNode(3);
+            tree.Right = createNode(6);
+            tree.Left.Left = createNode(2);
+            tree.Left.Right = createNode(4);
 
             // Print inorder traversal of the input tree
             Console.Write("Inorder of original tree: ");
             inorder(tree);
-            BTNode1 mirror = null;
+            TreeNode mirror = null;
             mirror = mirrorify(tree);
 
             // Print inorder traversal of the mirror tree
